406adb827a69510d241f84a9ff699372d27f8a65
Saman Siadati <siadatisaman@gmail.com>
2022-09-08 17:59:03 +1000

    the setup.py has been altered

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name='raz',
     packages=['raz'],
-    version='0.0.9',
+    version='0.1.0',
     license='MIT',
     description='Testing installation of Package',
     long_description=long_description,
